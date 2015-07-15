69236d891df87d029183ef7ed8db6bf37767cd0f
Hongcai Deng <admin@dhchouse.com>
2015-07-16 00:08:27 +0800

    successful!

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,40 +1,71 @@
+#include<Wire.h>
+int ledState = 0;
+int dist[4] = {0, 0, 0, 0};
+int reg = 0;
+
 unsigned int echoPin[4] = {2, 4, 6, 8};   // connect to Echo/RX
 unsigned int trigPin[4] = {3, 5, 7, 9};   // connect to Trig/TX
 
-void setup(){ 
-  Serial.begin(9600); 
-  Serial.setTimeout(10); // Serial read timeout
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin(8); // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveData);
+  for (int i = 0; i < 4; i++) {
+    pinMode(echoPin[i], INPUT);
+    pinMode(trigPin[i], OUTPUT);
+  }
+}
+
+int caculateDistance(int i)
+{
+  unsigned long time_echo_us = 0;
+  unsigned long dist_mm = 0;
+  digitalWrite(trigPin[i], LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin[i], HIGH);  // start pluse
+  delayMicroseconds(12);        // set pluse width
+  digitalWrite(trigPin[i], LOW);   // end pluse
+
+  time_echo_us = pulseIn(echoPin[i], HIGH);  // caculate pluse width
+  if ((time_echo_us < 60000) && (time_echo_us > 1)) // validate pluse width range
+  {
+    dist_mm = time_echo_us * 5 / 29; // caculate distance using pluse duration
+  }
+  return dist_mm;
+}
+
+void loop()
+{
+  if (ledState == 0) {
+    digitalWrite(13, HIGH); // set the LED on
+    ledState = 1;
+  }
+  else {
+    digitalWrite(13, LOW); // set the LED off
+    ledState = 0;
+  }
   for(int i = 0; i < 4; i++) {
-    pinMode(echoPin[i], INPUT);  
-    pinMode(trigPin[i], OUTPUT); 
+    dist[i] = caculateDistance(i);
+  }
+  Serial.println(reg);
+  Serial.println(dist[reg]);
+  delay(1000);
+}
+
+void requestEvent()
+{
+  byte tmp[2] = {0};
+  tmp[0] = highByte(dist[reg]);
+  tmp[1] = lowByte(dist[reg]); // BigEndian
+  Wire.write(tmp, 2);
+}
+
+void receiveData(int byteCount)
+{
+  while (Wire.available()) {
+    reg  = Wire.read();
   }
 }
 
-void loop(){
-  while(Serial.available() > 0) {
-
-    int no = Serial.parseInt();
-
-    if(no >= 4) {
-      continue;
-    }
-    
-    unsigned long time_echo_us = 0;
-    unsigned long dist_mm = 0;
-    
-    digitalWrite(trigPin[no], LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin[no], HIGH);  // start pluse
-    delayMicroseconds(12);        // set pluse width
-    digitalWrite(trigPin[no], LOW);   // end pluse
-    
-    time_echo_us = pulseIn(echoPin[no], HIGH);  // caculate pluse width
-    if((time_echo_us < 60000) && (time_echo_us > 1)) // validate pluse width range
-    {
-      dist_mm = time_echo_us*5/29;  // caculate distance using pluse duration
-      
-      Serial.print(dist_mm, DEC); 
-      Serial.println("");
-    }
-  }
-}
